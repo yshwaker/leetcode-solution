ba9370d4f200b5a4ab72b83f4df56c5a42ec1b3f
yshwaker <yshwaker@gmail.com>
2016-02-22 10:56:48 -0800

    revised format

--- 137-SingleNumberII**.py
+++ 137-SingleNumberII**.py
@@ -4,17 +4,21 @@ source: https://leetcode.com/problems/single-number-ii/
 date: 02-22-2016
 ----------------
 problem:
-Given an array of integers, every element appears three times except for one. Find that single one.
+Given an array of integers, every element appears three times except for one.
+Find that single one.
 
 Note:
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+Your algorithm should have a linear runtime complexity. Could you implement it
+without using extra memory?
 ----------------
 accumulate the value of each bit of each number and mod it by 3, the numbers
 left are the values of each bit of the specal number
 
 NOTE:
 for python, if the value of int overflows, the number will automatically
-converted to long. So we need to consider the negative numbers as special case.
+converted to long. Thus, value like 0b10000001111111111111111111111111, the
+sign bit will be considered as a normal bit of type long.
+So we need to consider the negative numbers as special case.
 
 """
 
