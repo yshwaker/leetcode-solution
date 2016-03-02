8f79a5e307e15c4400685d1cccdb45e32be80781
yshwaker <yshwaker@gmail.com>
2016-03-02 11:05:47 -0800

    add D&C method

--- 53-MaximumSubarray.py
+++ 53-MaximumSubarray.py
@@ -24,10 +24,43 @@ class Solution(object):
         :rtype: int
         """
         # O(n) dynamic programming
-        #
-        # max_sum = nums[0]
-        # cur_sum = nums[0]
-        # for i in range(1, len(nums)):
-        #     cur_sum = max(cur_sum + nums[i], nums[i])
-        #     max_sum = max(max_sum, cur_sum)
-        # return max_sum
+        max_sum = nums[0]
+        cur_sum = nums[0]
+        for i in range(1, len(nums)):
+            cur_sum = max(cur_sum + nums[i], nums[i])
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
+
+    # divide and conquer version, O(nlgn)
+    # divide: O(1)
+    # combine: O(n)
+    # according to master theorem, the complexity of this alg is O(nlogn)
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        sum = self.findMax(0, len(nums) - 1, nums)
+        return sum
+
+    def findMax(self, left, right, nums):
+        if left < right:
+            mid = left + (right - left) / 2
+            maxL = self.findMax(left, mid, nums)
+            maxR = self.findMax(mid + 1, right, nums)
+            max_sum = max(maxL, maxR)
+            maxM = nums[mid]
+            sumM = nums[mid]
+            for i in range(mid - 1, left - 1, -1):
+                sumM += nums[i]
+                if sumM > maxM:
+                    maxM = sumM
+            sumM = maxM
+            for i in range(mid + 1, right + 1):
+                sumM += nums[i]
+                if sumM > maxM:
+                    maxM = sumM
+            max_sum = max(max_sum, maxM)
+            return max_sum
+        if left == right:
+            return nums[left]
